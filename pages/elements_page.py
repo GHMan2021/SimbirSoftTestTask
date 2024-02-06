510be85a011a36c28de3b4559a0a276f7e0a0642
YURI <statio2009@gmail.com>
2024-02-06 13:22:49 +0400

    add allure-pytest and allure descriptions into elements_test.py and elements_page.py add allure-pytest to requirements.txt

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,5 +1,7 @@
 import random
 import time
+
+import allure
 from selenium.webdriver import Keys
 
 from generator.generator import generated_student, generated_date
@@ -10,6 +12,7 @@ from pages.base_page import BasePage
 class FormPage(BasePage):
     locators = FormPageLocators()
 
+    @allure.title('Fill form fields')
     def fill_fields_and_submit(self):
         student_data = next(generated_student())
 
@@ -22,77 +25,83 @@ class FormPage(BasePage):
         state_and_city = student_data.state_and_city
 
         self.remove_footer()
-        self.element_is_visible(self.locators.FIRST_NAME).send_keys(first_name)
-        self.element_is_visible(self.locators.LAST_NAME).send_keys(last_name)
-        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        with allure.step('Fill full name and email'):
+            self.element_is_visible(self.locators.FIRST_NAME).send_keys(first_name)
+            self.element_is_visible(self.locators.LAST_NAME).send_keys(last_name)
+            self.element_is_visible(self.locators.EMAIL).send_keys(email)
 
-        gender = self.element_is_visible(self.locators.GENDER_RADIOBUTTON)
-        gender.click()
-        student_data.gender = gender.text
+        with allure.step('Select gender'):
+            gender = self.element_is_visible(self.locators.GENDER_RADIOBUTTON)
+            gender.click()
+            student_data.gender = gender.text
 
-        self.element_is_visible(self.locators.MOBILE).send_keys(mobile)
+        with allure.step('Fill mobile'):
+            self.element_is_visible(self.locators.MOBILE).send_keys(mobile)
 
-        # select date of birth
-        date = next(generated_date())
-        self.element_is_visible(self.locators.DATE_INPUT).click()
-        self.select_date_by_text(self.locators.DATE_SELECT_YEAR, date.year)
-        self.select_date_by_text(self.locators.DATE_SELECT_MONTH, date.month)
-        self.select_date_item_from_list(self.locators.DATE_SELECT_DAY_LIST, date.day)
-        student_data.date_of_birth = date
+        with allure.step('Select date of birth'):
+            date = next(generated_date())
+            self.element_is_visible(self.locators.DATE_INPUT).click()
+            self.select_date_by_text(self.locators.DATE_SELECT_YEAR, date.year)
+            self.select_date_by_text(self.locators.DATE_SELECT_MONTH, date.month)
+            self.select_date_item_from_list(self.locators.DATE_SELECT_DAY_LIST, date.day)
+            student_data.date_of_birth = date
 
-        # select subjects
-        count_of_subjects = random.randint(0, 14)
-        if count_of_subjects > 0:
-            random.shuffle(subjects)
-            student_subjects = subjects[:count_of_subjects]
-            for subject in student_subjects:
-                element = self.element_is_visible(self.locators.SUBJECTS)
-                element.send_keys(subject)
-                element.send_keys(Keys.RETURN)
-            student_data.subjects = student_subjects
-        else:
-            student_data.subjects = ['']
+        with allure.step('Select subjects'):
+            count_of_subjects = random.randint(0, 14)
+            if count_of_subjects > 0:
+                random.shuffle(subjects)
+                student_subjects = subjects[:count_of_subjects]
+                for subject in student_subjects:
+                    element = self.element_is_visible(self.locators.SUBJECTS)
+                    element.send_keys(subject)
+                    element.send_keys(Keys.RETURN)
+                student_data.subjects = student_subjects
+            else:
+                student_data.subjects = ['']
 
-        # select hobbies
-        count_of_hobbies = random.randint(0, 3)
-        if count_of_hobbies > 0:
-            hobbies = self.elements_are_visible(self.locators.HOBBIES_LIST)
-            random.shuffle(hobbies)
-            student_hobbies = hobbies[:count_of_hobbies]
-            for hobby in student_hobbies:
-                hobby.click()
-                time.sleep(1)
-            student_data.hobbies = [i.text for i in student_hobbies]
-        else:
-            student_data.hobbies = ['']
+        with allure.step('Select hobbies'):
+            count_of_hobbies = random.randint(0, 3)
+            if count_of_hobbies > 0:
+                hobbies = self.elements_are_visible(self.locators.HOBBIES_LIST)
+                random.shuffle(hobbies)
+                student_hobbies = hobbies[:count_of_hobbies]
+                for hobby in student_hobbies:
+                    hobby.click()
+                    time.sleep(1)
+                student_data.hobbies = [i.text for i in student_hobbies]
+            else:
+                student_data.hobbies = ['']
 
-        # upload file
-        upload_file = self.element_is_visible(self.locators.UPLOAD_FILE)
-        upload_file.send_keys(r'C:\Users\YURI\PycharmProjects\SimbirSoftTestTask\tests\picture.jpg')
-        student_data.upload_file = "picture.jpg"
+        with allure.step('Upload file'):
+            upload_file = self.element_is_visible(self.locators.UPLOAD_FILE)
+            upload_file.send_keys(r'C:\Users\YURI\PycharmProjects\SimbirSoftTestTask\tests\picture.jpg')
+            student_data.upload_file = "picture.jpg"
 
-        self.element_is_visible(self.locators.CUR_ADDRESS).send_keys(cur_address)
+        with allure.step('Fill current address'):
+            self.element_is_visible(self.locators.CUR_ADDRESS).send_keys(cur_address)
 
-        # select state and city
         state_and_city_list = [(k, v) for k, v in state_and_city.items()]
         state_name, cities_list = random.choice(state_and_city_list)
         city_name = random.choice(cities_list)
 
-        state = self.element_is_present(self.locators.STATE_LIST)
-        state.send_keys(state_name)
-        state.send_keys(Keys.RETURN)
-        student_data.state = state_name
+        with allure.step('Select state'):
+            state = self.element_is_present(self.locators.STATE_LIST)
+            state.send_keys(state_name)
+            state.send_keys(Keys.RETURN)
+            student_data.state = state_name
 
-        city = self.element_is_present(self.locators.CITY_LIST)
-        city.send_keys(city_name)
-        city.send_keys(Keys.RETURN)
-        student_data.city = city_name
+        with allure.step('Select city'):
+            city = self.element_is_present(self.locators.CITY_LIST)
+            city.send_keys(city_name)
+            city.send_keys(Keys.RETURN)
+            student_data.city = city_name
 
-        # submit button
-        self.element_is_visible(self.locators.SUBMIT).click()
+        with allure.step('Click submit button'):
+            self.element_is_visible(self.locators.SUBMIT).click()
 
         return student_data
 
+    @allure.title('Get form result')
     def form_result(self):
         result_list = self.elements_are_visible(self.locators.RESULT_TABLE)
         result_text = [i.text for i in result_list]
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,22 +1,38 @@
 from pages.elements_page import FormPage
+import allure
 
 
+@allure.suite('Form elements')
 class TestFormElements:
-
+    @allure.feature('Test Form elements')
     def test_form(self, driver):
-        form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
-        form_page.open()
-        output = form_page.fill_fields_and_submit()
-        result_table = form_page.form_result()
+        with allure.step('Open browser'):
+            form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
+            form_page.open()
+        with allure.step('Get output data'):
+            output = form_page.fill_fields_and_submit()
+        with allure.step('Get result table data'):
+            result_table = form_page.form_result()
 
-        assert f"{output.first_name} {output.last_name}" == result_table[0], 'full name does not matched'
-        assert output.email == result_table[1], 'email does not matched'
-        assert output.gender == result_table[2], 'gender does not matched'
-        assert output.mobile == result_table[3], 'mobile does not matched'
-        assert (f"{output.date_of_birth.day} {output.date_of_birth.month},{output.date_of_birth.year}" ==
-                result_table[4]), 'date_of_birth does not matched'
-        assert output.subjects == list(result_table[5].split(', ')), 'subjects does not matched'
-        assert output.hobbies == list(result_table[6].split(', ')), 'hobbies does not matched'
-        assert output.upload_file == result_table[7], 'upload file does not matched'
-        assert output.cur_address == result_table[8], 'current address does not matched'
-        assert f"{output.state} {output.city}" == result_table[9], 'state and city does not matched'
+        with allure.step('Check data form'):
+            with allure.step('Check full name'):
+                assert f"{output.first_name} {output.last_name}" == result_table[0], 'full name does not matched'
+            with allure.step('Check email'):
+                assert output.email == result_table[1], 'email does not matched'
+            with allure.step('Check gender'):
+                assert output.gender == result_table[2], 'gender does not matched'
+            with allure.step('Check mobile number'):
+                assert output.mobile == result_table[3], 'mobile does not matched'
+            with allure.step('Check date of birth'):
+                assert (f"{output.date_of_birth.day} {output.date_of_birth.month},{output.date_of_birth.year}" ==
+                        result_table[4]), 'date_of_birth does not matched'
+            with allure.step('Check subjects'):
+                assert output.subjects == list(result_table[5].split(', ')), 'subjects does not matched'
+            with allure.step('Check hobbies'):
+                assert output.hobbies == list(result_table[6].split(', ')), 'hobbies does not matched'
+            with allure.step('Check upload file'):
+                assert output.upload_file == result_table[7], 'upload file does not matched'
+            with allure.step('Check address'):
+                assert output.cur_address == result_table[8], 'current address does not matched'
+            with allure.step('Check state and city'):
+                assert f"{output.state} {output.city}" == result_table[9], 'state and city does not matched'
