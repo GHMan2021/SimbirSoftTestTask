5a34da612046b23c8561361f04f4e126441aa03f
YURI <statio2009@gmail.com>
2024-03-15 01:14:07 +0400

    modify generator, add allure discription elements_test.py

--- generators/generator.py
+++ generators/generator.py
@@ -10,7 +10,7 @@ subjects_list = [
     "Economics", "Arts", "Social Studies", "History", "Civics"
 ]
 
-hobbies_list = ['Sports', 'Reading', 'Music']
+hobbies_list = ["Sports", "Reading", "Music"]
 
 state_and_city_dict = {
     "NCR": ["Delhi", "Gurgaon", "Noida"],
@@ -26,28 +26,34 @@ class StudentGenerator:
         first_name = fake.first_name()
         last_name = fake.last_name()
         email = fake.email()
-        gender = random.choice(['Male', 'Female', 'Other'])
-        mobile = fake.numerify('##########')
+        gender = random.choice(["Male", "Female", "Other"])
+        mobile = fake.numerify("##########")
         date_of_birth = DateGenerator.random()
-        subjects = get_random_items(subjects_list)
-        hobbies = get_random_items(hobbies_list)
-        upload_file = 'picture.jpg'
+
+        subjects_tmp = [i for i in subjects_list if random.choice([True, False])]
+        subjects = subjects_tmp if bool(subjects_tmp) else ['']
+
+        hobbies_tmp = [i for i in hobbies_list if random.choice([True, False])]
+        hobbies = hobbies_tmp if bool(hobbies_tmp) else ['']
+
+        upload_file = "picture.jpg"
         cur_address = fake.street_address()
+
         state_and_city = random.choice([(k, v) for k, v in state_and_city_dict.items()])
         state, city = state_and_city[0], random.choice(state_and_city[1])
         return {
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email,
-            'gender': gender,
-            'mobile': mobile,
-            'date_of_birth': date_of_birth,
-            'subjects': subjects,
-            'hobbies': hobbies,
-            'upload_file': upload_file,
-            'cur_address': cur_address,
-            'state': state,
-            'city': city
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "gender": gender,
+            "mobile": mobile,
+            "date_of_birth": date_of_birth,
+            "subjects": subjects,
+            "hobbies": hobbies,
+            "upload_file": upload_file,
+            "cur_address": cur_address,
+            "state": state,
+            "city": city
         }
 
 
@@ -58,17 +64,7 @@ class DateGenerator:
         month = fake.month_name()
         day = fake.day_of_month()
         return {
-            'year': year,
-            'month': month,
-            'day': day
+            "year": year,
+            "month": month,
+            "day": day
         }
-
-
-def get_random_items(items, min_choice=0):
-    max_choice = len(items)
-    count_of_items = random.randint(min_choice, max_choice)
-    random_items = ['']
-    if count_of_items > 0:
-        random.shuffle(items)
-        random_items = items[:count_of_items]
-    return random_items
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -3,58 +3,73 @@ from pages.elements_page import FormPage
 import allure
 
 
-@allure.suite('Form elements')
-class TestFormElements:
-    @allure.feature('Test Form elements')
-    def test_form(self, driver):
-        with allure.step('Open browser'):
-            form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
-            form_page.open()
+@allure.feature('demoqa.com')
+@allure.title('Test Form elements')
+@allure.description("""
+    Goal: Testing form elements
 
-        form_page.remove_footer()
+    Steps:
+    1. Open url 'https://demoqa.com/automation-practice-form'
+    2. Fill all fields
+    3. Get result table data
+    4. Check full name
+    5. Check email
+    6. Check gender
+    7. Check mobile number
+    8. Check date of birth
+    9. Check subjects
+    10. Check hobbies
+    11. Check upload file
+    12. Check address
+    13. Check state and city
+""")
+def test_form(driver):
+    with allure.step('Open browser'):
+        form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
+        form_page.open()
 
-        student_data = StudentGenerator.random()
-        with allure.step('Fill all fields'):
-            form_page.enter_first_name(student_data['first_name'])
-            form_page.enter_last_name(student_data['last_name'])
-            form_page.enter_email(student_data['email'])
-            form_page.enter_gender(student_data['gender'])
-            form_page.enter_mobile(student_data['mobile'])
-            form_page.enter_date_of_birth(student_data['date_of_birth'])
-            form_page.enter_subjects(student_data['subjects'])
-            form_page.enter_hobbies(student_data['hobbies'])
-            form_page.enter_picture(student_data['upload_file'])
-            form_page.enter_cur_address(student_data['cur_address'])
-            form_page.enter_state(student_data['state'])
-            form_page.enter_city(student_data['city'])
-        form_page.enter_submit()
+    form_page.remove_footer()
 
-        with allure.step('Get result table data'):
-            result_data = form_page.form_result()
+    student_data = StudentGenerator.random()
+    with allure.step('Fill all fields'):
+        form_page.enter_first_name(student_data['first_name'])
+        form_page.enter_last_name(student_data['last_name'])
+        form_page.enter_email(student_data['email'])
+        form_page.enter_gender(student_data['gender'])
+        form_page.enter_mobile(student_data['mobile'])
+        form_page.enter_date_of_birth(student_data['date_of_birth'])
+        form_page.enter_subjects(student_data['subjects'])
+        form_page.enter_hobbies(student_data['hobbies'])
+        form_page.enter_picture(student_data['upload_file'])
+        form_page.enter_cur_address(student_data['cur_address'])
+        form_page.enter_state(student_data['state'])
+        form_page.enter_city(student_data['city'])
+    form_page.enter_submit()
 
-        with allure.step('Check data form'):
-            with allure.step('Check full name'):
-                assert (f"{student_data['first_name']} {student_data['last_name']}" == result_data['Student Name']), \
-                    'full name does not matched'
-            with allure.step('Check email'):
-                assert student_data['email'] == result_data['Student Email'], 'email does not matched'
-            with allure.step('Check gender'):
-                assert student_data['gender'] == result_data['Gender'], 'gender does not matched'
-            with allure.step('Check mobile number'):
-                assert student_data['mobile'] == result_data['Mobile'], 'mobile does not matched'
-            with allure.step('Check date of birth'):
-                date_dict = student_data['date_of_birth']
-                assert f"{date_dict['day']} {date_dict['month']},{date_dict['year']}" == result_data['Date of Birth'], \
-                    'date_of_birth does not matched'
-            with allure.step('Check subjects'):
-                assert student_data['subjects'] == list(result_data['Subjects'].split(", ")), \
-                    'subjects does not matched'
-            with allure.step('Check hobbies'):
-                assert student_data['hobbies'] == list(result_data['Hobbies'].split(", ")), 'hobbies does not matched'
-            with allure.step('Check upload file'):
-                assert student_data['upload_file'] == result_data['Picture'], 'upload file does not matched'
-            with allure.step('Check address'):
-                assert student_data['cur_address'] == result_data['Address'], 'current address does not matched'
-            with allure.step('Check state and city'):
-                assert f"{student_data['state']} {student_data['city']}" == result_data['State and City'], \
-                    'state and city does not matched'
+    with allure.step('Get result table data'):
+        result_data = form_page.form_result()
+
+    with allure.step('Check full name'):
+        assert (f"{student_data['first_name']} {student_data['last_name']}" == result_data['Student Name']), \
+            'full name does not matched'
+    with allure.step('Check email'):
+        assert student_data['email'] == result_data['Student Email'], 'email does not matched'
+    with allure.step('Check gender'):
+        assert student_data['gender'] == result_data['Gender'], 'gender does not matched'
+    with allure.step('Check mobile number'):
+        assert student_data['mobile'] == result_data['Mobile'], 'mobile does not matched'
+    with allure.step('Check date of birth'):
+        date_dict = student_data['date_of_birth']
+        assert f"{date_dict['day']} {date_dict['month']},{date_dict['year']}" == result_data['Date of Birth'], \
+            'date of birth does not matched'
+    with allure.step('Check subjects'):
+        assert student_data['subjects'] == list(result_data['Subjects'].split(", ")), 'subjects does not matched'
+    with allure.step('Check hobbies'):
+        assert student_data['hobbies'] == list(result_data['Hobbies'].split(", ")), 'hobbies does not matched'
+    with allure.step('Check upload file'):
+        assert student_data['upload_file'] == result_data['Picture'], 'upload file does not matched'
+    with allure.step('Check address'):
+        assert student_data['cur_address'] == result_data['Address'], 'current address does not matched'
+    with allure.step('Check state and city'):
+        assert f"{student_data['state']} {student_data['city']}" == result_data['State and City'], \
+            'state and city does not matched'
